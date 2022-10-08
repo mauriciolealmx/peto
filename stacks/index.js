a29c52dfd08b481cd9edd547120e6bff18cef730
Mauricio Leal <mauricio.leal@motorolasolutions.com>
2022-10-08 15:14:53 -0500

    initial sst commit

---
+++ stacks/index.js
@@ -0,0 +1,14 @@
+import { App } from '@serverless-stack/resources';
+
+/**
+ * @param {App} app
+ */
+export default function (app) {
+  app.setDefaultFunctionProps({
+    runtime: 'nodejs16.x',
+    srcPath: 'services',
+    bundle: {
+      format: 'esm',
+    },
+  });
+}
