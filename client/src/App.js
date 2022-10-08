91cba94cbff0956505af594d1cf100c79fa42c3b
Mauricio Leal <mauricio.leal@motorolasolutions.com>
2022-10-08 16:28:26 -0500

    client: add nav links

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 import Routes from './Routes';
-
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
 
 const App = () => {
   return (
     <Container>
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
         <Container>
-          <Navbar.Brand className="font-weight-bold text-muted">
-            Pito Escurre
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand className="font-weight-bold text-muted">
+              Pito Escurre
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav activeKey={window.location.pathname}>
+              <LinkContainer to="/new">
+                <Nav.Link>Create</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
       <Routes />
