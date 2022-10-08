1f4ac616b06a0c9f7cde1789f2d2858a7fb11b7b
Mauricio Leal <mauricio.leal@motorolasolutions.com>
2022-10-08 16:30:14 -0500

    client: add 404 page

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes as RDRoutes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound';
 
 const Routes = () => {
   return (
     <RDRoutes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />;
     </RDRoutes>
   );
 };
---
+++ client/src/pages/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './NotFound.css';
+
+const NotFound = () => {
+  return (
+    <div className="NotFound text-center">
+      <h3>Sorry, page not found!</h3>
+    </div>
+  );
+};
+
+export default NotFound;
