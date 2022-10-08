ac11998757931c70624ce7bb12f94d0b3a8c5168
Mauricio Leal <mauricio.leal@motorolasolutions.com>
2022-10-08 16:15:35 -0500

    client: create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        <Container>
+          <Navbar.Brand className="font-weight-bold text-muted">
+            Pito Escurre
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Container>
+      </Navbar>
+    </Container>
   );
-}
+};
 
 export default App;
