6eb648bc7fe8cdd7b0389797230ce7adfdb3e002
Mauricio Leal <mauricio.leal@motorolasolutions.com>
2022-10-08 22:28:23 -0500

    client: add recoil user atom

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
@@ -13,9 +14,11 @@ import './config/setAmplify.config';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <App />
+      </Router>
+    </RecoilRoot>
   </React.StrictMode>
 );
 
