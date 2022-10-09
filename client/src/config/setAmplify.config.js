590f102b4b67e29888ec20ea9bd5b12dec9ae7fa
Mauricio Leal <mauricio.leal@motorolasolutions.com>
2022-10-08 22:07:10 -0500

    client: update amplify config

--- client/src/config/setAmplify.config.js
+++ client/src/config/setAmplify.config.js
@@ -3,6 +3,13 @@ import { Amplify } from 'aws-amplify';
 import config from './aws-amplify.config';
 
 Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
   Storage: {
     region: config.s3.REGION,
     bucket: config.s3.BUCKET,
