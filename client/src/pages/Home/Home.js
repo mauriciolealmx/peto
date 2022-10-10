030c093abb06a1b478cb3f27023484f1bd429980
Mauricio Leal <mauricio.leal@motorolasolutions.com>
2022-10-10 15:45:12 -0500

    client: show images on Home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,48 @@
+import { useEffect, useState } from 'react';
+import { API, Storage } from 'aws-amplify';
+import { useRecoilValue } from 'recoil';
+
+import Image from 'react-bootstrap/Image';
+import Stack from 'react-bootstrap/Stack';
+
+import userState from '../../atoms/user.atom';
+
+import './Home.css';
 
 const Home = () => {
+  const [allPosts, setPosts] = useState([]);
+  const isAuthenticated = useRecoilValue(userState);
+
+  useEffect(() => {
+    const onLoad = async () => {
+      if (!isAuthenticated) {
+        return;
+      }
+
+      try {
+        const posts = await API.get('posts', '/posts');
+        const postsPromises = posts.map(async (post) => ({
+          ...post,
+          attachmentURL: await Storage.vault.get(post.imageURL),
+        }));
+
+        const postsWithURL = await Promise.all(postsPromises);
+        setPosts(postsWithURL);
+      } catch (e) {}
+    };
+
+    onLoad();
+  }, [isAuthenticated, setPosts]);
+
   return (
     <div className="Home">
-      <div className="lander">
-        <h1>Peto</h1>
-        <p className="text-muted">A simple note taking app</p>
-      </div>
+      {allPosts.length > 0 && (
+        <Stack className="images-root" direction="horizontal">
+          {allPosts.map((post) => (
+            <Image src={post.attachmentURL} />
+          ))}
+        </Stack>
+      )}
     </div>
   );
 };
