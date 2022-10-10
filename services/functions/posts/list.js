85e5e73220553f5f65cff5674ca87387da9f31da
Mauricio Leal <mauricio.leal@motorolasolutions.com>
2022-10-10 10:29:44 -0500

    server: update list query to use userId

--- services/functions/posts/list.js
+++ services/functions/posts/list.js
@@ -1,12 +1,12 @@
 import handler from '../../utils/handler';
 import dynamoDb from '../../utils/dynamodb';
 
-export const main = handler(async () => {
+export const main = handler(async (event) => {
   const params = {
     TableName: process.env.TABLE_NAME,
     KeyConditionExpression: 'userId = :userId',
     ExpressionAttributeValues: {
-      ':userId': 'mvp-user',
+      ':userId': event.requestContext.authorizer.iam.cognitoIdentity.identityId,
     },
   };
 
