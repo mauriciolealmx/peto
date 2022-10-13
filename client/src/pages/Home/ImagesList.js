c38690af9e0724cf034d67669bf132bdc73b1820
Mauricio Leal <mauricio.leal@motorolasolutions.com>
2022-10-13 11:11:51 -0500

    client: wip add card

--- client/src/pages/Home/ImagesList.js
+++ client/src/pages/Home/ImagesList.js
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 
+import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
+import Stack from 'react-bootstrap/Stack';
 
 const exp = new RegExp(/.mp4$/, 'i');
 const isVideo = (url) => {
@@ -20,11 +22,26 @@ const ImagesList = ({ posts }) => {
         <source src={post.attachmentURL} type="video/mp4" />
       </video>
     ) : (
-      <Image
-        key={post.attachmentURL}
-        src={post.attachmentURL}
-        onClick={() => handleImageClick(post.postId)}
-      />
+      <Card>
+        <Image
+          key={post.attachmentURL}
+          src={post.attachmentURL}
+          onClick={() => handleImageClick(post.postId)}
+        />
+        <Card.Body>
+          <Stack className="titles" direction="horizontal">
+            <Card.Title>Auto Repair Center</Card.Title>
+            <Card.Title>$1M</Card.Title>
+          </Stack>
+          <Card.Subtitle className="mb-2 text-muted">
+            Dallas, TX
+          </Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     )
   );
 };
